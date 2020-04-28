654dba6e3434a7a75775f3fd7290605ef51140bc
Dennis Wiest <dw_8@web.de>
2020-04-28 13:41:07 +0200

    Polished coordinate conversion from coordinatesystempoint to canvaspoint

--- Model/CoordinateConverter.cs
+++ Model/CoordinateConverter.cs
@@ -15,8 +15,170 @@ namespace LinearTransformation.Model {
             throw new NotImplementedException();
 
         }
+        private static double CalculateX(double min, double max, double coordinate) {
+            // case 1: Min < 0 < Max
+            if (min < 0 && 0 < max) {
+                if (coordinate == 0) {
+                    // Ursprung
+                    return -min;
+                } else if (coordinate > 0) {
+                    //Ursprung + coordinate
+                    return -min + coordinate;
+                } else if (coordinate < 0) {
+                    // Ursprung + (negative) coordinate
+                    return -min + coordinate;
+                }
+            }
 
-        public static Vector FromCoordinateSystemPointToCanvasPoint(Canvas canvas, CoordinateSystemData coordinateSystemData, Vector point) {
+            // case 2: 0 < Min < Max
+            if (0 < min) {
+                if (coordinate == 0) {
+                    // Ursprung
+                    return -min;
+                } else if (coordinate > 0) {
+                    // Ursprung + coordinate
+                    return -min + coordinate;
+                } else if (coordinate < 0) {
+                    // Ursprung + (negative) coordinate)
+                    return -min + coordinate;
+                }
+            }
+
+            // case 3: Min < Max < 0
+            if (0 > max) {
+                if (coordinate == 0) {
+                    // Ursprung
+                    return -min;
+                } else if (coordinate > 0) {
+                    // Ursprung + coordinate
+                    return -min + coordinate;
+                } else if (coordinate < 0) {
+                    // Ursprung + (negative) coordinate
+                    return -min + coordinate;
+                }
+            }
+
+            // case 4: (Min = 0) < Max
+            if (min == 0) {
+                if (coordinate == 0) {
+                    // Ursprung
+                    return -min;
+                } else if (coordinate > 0) {
+                    // Ursprung + coordinate
+                    return -min + coordinate;
+                } else if (coordinate < 0) {
+                    // Urpsrung + (negative coordinate)
+                    return -min + coordinate;
+                }
+            }
+
+            // case 5: Min < (Max = 0)
+            if (max == 0) {
+                if (coordinate == 0) {
+                    // Ursprung
+                    return -min;
+                } else if (coordinate > 0) {
+                    // Ursprung + coordinate
+                    return -min + coordinate;
+                } else if (coordinate < 0) {
+                    // Ursprung + (negative) coordinate;
+                    return -min + coordinate;
+                }
+            }
+
+            throw new Exception("invalid range");
+        }
+
+        private static double CalculateY(double min, double max, double coordinate) {
+            // case 1: Min < 0 < Max
+            if (min < 0 && 0 < max) {
+                if (coordinate == 0) {
+                    // Ursprung
+                    return max;
+                } else if (coordinate > 0) {
+                    //Ursprung - coordinate
+                    return max - coordinate;
+                } else if (coordinate < 0) {
+                    // Ursprung - (negative) coordinate
+                    return max - coordinate;
+                }
+            }
+
+            // case 2: 0 < Min < Max
+            if (0 < min) {
+                if (coordinate == 0) {
+                    // Ursprung
+                    return max;
+                } else if (coordinate > 0) {
+                    // Ursprung - coordinate
+                    return max - coordinate;
+                } else if (coordinate < 0) {
+                    // Ursprung - (negative) coordinate)
+                    return max - coordinate;
+                }
+            }
+
+            // TODO: CHECK THIS OUT - THIS ALGORITHM USES -MAX INSTEAD OF MAX
+            // case 3: Min < Max < 0
+            if (0 > max) {
+                if (coordinate == 0) {
+                    // Ursprung
+                    return -max;
+                } else if (coordinate > 0) {
+                    // Ursprung - coordinate
+                    return -max - coordinate;
+                } else if (coordinate < 0) {
+                    // Ursprung - (negative) coordinate
+                    return -max - coordinate;
+                }
+            }
+
+            // case 4: (Min=0) < Max
+            if (min == 0) {
+                if (coordinate == 0) {
+                    // Ursprung
+                    return max;
+                } else if (coordinate > 0) {
+                    // Ursprung - coordinate
+                    return max - coordinate;
+                } else if (coordinate < 0) {
+                    // Urpsrung - (negative coordinate)
+                    return max - coordinate;
+                }
+            }
+
+            // case 5: Min < (Max = 0)
+            if (max == 0) {
+                if (coordinate == 0) {
+                    // Ursprung
+                    return max;
+                } else if (coordinate > 0) {
+                    // Ursprung - coordinate
+                    return max - coordinate;
+                } else if (coordinate < 0) {
+                    // Ursprung + (negative) coordinate;
+                    return max - coordinate;
+                }
+            }
+
+            throw new Exception("invalid range");
+        }
+
+        public static Vector FromCoordinateSystemPointToCanvasPoint(Canvas canvas, CoordinateSystemData coordinateSystemData, Vector coordinate) {
+            double w = canvas.ActualWidth;
+            double h = canvas.ActualHeight;
+
+            if (h == 0 || w == 0 || double.IsNaN(w) || double.IsNaN(h)) {
+                throw new Exception("Why is my canvas not working?");
+            }
+
+            Size cellSize = new Size{
+                Width = w / coordinateSystemData.GetCellSize().Width,
+                Height = h / coordinateSystemData.GetCellSize().Height,
+            };
+
+            return new Vector(CalculateX(coordinateSystemData.MinX, coordinateSystemData.MaxX, coordinate.X) * cellSize.Width,
+                              CalculateY(coordinateSystemData.MinY, coordinateSystemData.MaxY, coordinate.Y) * cellSize.Height);
             #region
             //if (point.X > 0) {
             //    double x = point.X + Math.Abs(coordinateSystemData.MinX) + 1; // +1 because of y = 0
@@ -55,7 +217,7 @@ namespace LinearTransformation.Model {
             //    }
             //}
             #endregion
-
+            /*
             // TODO: ANPASSEN FÜR ALLE SONDERFÄLLE
 
             // Zusammengefasst:
@@ -96,7 +258,7 @@ namespace LinearTransformation.Model {
             position.X *= cellSize.Width;
             position.Y *= cellSize.Height;
 
-            return position;
+            return position;*/
         }
     }
 }
